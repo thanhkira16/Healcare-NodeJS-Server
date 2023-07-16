f0f297d6d4fe699d26709f0c49008c79b9e6c949
thanhkira16 <hthanh16092004@gmail.com>
2023-07-16 12:55:06 +0700

    test render data from db

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,16 @@
-import express from "express"
+import db from '../models/index';
+
+
 // đừng bỏ dấu / 
-let getHomePage = (req, res) => {
-    return res.render('homepage.ejs');
+let getHomePage = async(req, res) => {
+    try {
+        let data = await db.User.findAll();
+        return res.render('homepage.ejs', {
+            data: JSON.stringify(data)
+        });
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 
